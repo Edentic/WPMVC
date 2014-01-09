b708eb9396b1faa413d997b68de685e19d5ea5e8
kasperbaago <kapper14@gmail.com>
2014-01-09 15:22:40 +0100

    Method added for making a custom query to postType

--- Core/model/CustomPostModel.php
+++ Core/model/CustomPostModel.php
@@ -188,6 +188,38 @@ abstract class CustomPostModel {
         return $o;
     }
 
+    public static function getPosts($args = array()) {
+        $defaults = array(
+            'numberposts' => -1, 'offset' => 0,
+            'category' => 0, 'orderby' => 'date',
+            'order' => 'DESC', 'include' => array(),
+            'exclude' => array(), 'meta_key' => '',
+            'meta_value' =>'', 'post_type' => static::$customPostName,
+            'suppress_filters' => true,
+            'post_per_page' => -1
+        );
+
+        $r = wp_parse_args( $args, $defaults );
+        if ( empty( $r['post_status'] ) )
+            $r['post_status'] = ( 'attachment' == $r['post_type'] ) ? 'inherit' : 'publish';
+        if ( ! empty($r['numberposts']) && empty($r['posts_per_page']) )
+            $r['posts_per_page'] = $r['numberposts'];
+        if ( ! empty($r['category']) )
+            $r['cat'] = $r['category'];
+        if ( ! empty($r['include']) ) {
+            $incposts = wp_parse_id_list( $r['include'] );
+            $r['posts_per_page'] = count($incposts);  // only the number of posts included
+            $r['post__in'] = $incposts;
+        } elseif ( ! empty($r['exclude']) )
+            $r['post__not_in'] = wp_parse_id_list( $r['exclude'] );
+
+        $r['ignore_sticky_posts'] = true;
+        $r['no_found_rows'] = true;
+
+        $get_posts = new \WP_Query($r);
+        return $get_posts;
+    }
+
     /**
      * Saves post to database
      */
@@ -228,6 +260,10 @@ abstract class CustomPostModel {
        }
 
         return true;
+    }
+
+    public static function getTotalNumberOfItems() {
+        return \wp_count_posts(strtolower(static::$customPostName));
     }
 
     /**
